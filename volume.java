841ea92b3604d23807abf981aa6e7ec1f5c0c42b
sandeep-kd <51795742+sandeep-kd@users.noreply.github.com>
2019-06-18 22:44:06 +0530

    Add files via upload

---
+++ volume.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Volume
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		double r,vol;
+		System.out.println("Enter the radius");
+		r = sc.nextDouble();
+		vol=(4*3.14*r*r*r)/3;
+		System.out.println("The volume of the sphere is:  " +vol);
+	}
+}
